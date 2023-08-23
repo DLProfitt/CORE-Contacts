c95a03d036c05a38bd63c05a850380013e9d3708
David Profitt <dlprofitt@gmail.com>
2023-08-22 23:56:40 -0400

    Styled AuthPage

--- RefactorResume/client/src/components/AuthPage.js
+++ RefactorResume/client/src/components/AuthPage.js
@@ -3,26 +3,38 @@ import { ScrollableComponent } from "../utils/addFunction.js";
 import { LoginForm } from "./LoginForm.js";
 import "../index.css"
 import { RegistrationForm } from "./RegistrationForm.js";
+import logo from "../assets/core-logo.png";
 
 export default function AuthPage() {
     const [showLoginForm, setShowLoginForm] = useState(true);
 
     return (
-        <div>
-            <div id="sidebar">
-                <h1>Refactor(Resume)</h1>
-            </div>
-            <ScrollableComponent>
-                <div id="detail">
-                    {/*<LoginForm />*/}
-                    {showLoginForm ? <LoginForm /> : <RegistrationForm />}
+        <div id="root">
+            <>
+                <div id="sidebar">
+                    <img src={logo} alt="logo" />
+                    <h1>Getting Started</h1>
+                    <p>How to login:</p>
+                    <ul id="right-sidebar">
+                        <li>
+                            Email: alex.doe@example.com
+                        </li>
+                        <li>
+                            Password: password1
+                        </li>
+                    </ul>
+                    <button onClick={() => setShowLoginForm(true)}>Login</button>
+                    <button onClick={() => setShowLoginForm(false)}>Register</button>
                 </div>
-            </ScrollableComponent>
-            <div id="right-sidebar">
-                <h1>Login</h1>
-                <button onClick={() => setShowLoginForm(true)}>Login</button>
-                <button onClick={() => setShowLoginForm(false)}>Register</button>
-            </div>
+                <ScrollableComponent>
+                    <div id="detail">
+                        {/*<LoginForm />*/}
+                        {showLoginForm ? <LoginForm /> : <RegistrationForm />}
+                    </div>
+                </ScrollableComponent>
+                <div id="right-sidebar">
+                </div>
+            </>
         </div>
     );
 }
